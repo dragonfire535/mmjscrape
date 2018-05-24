d2260ad1411519db2c1cc76adc9f8b601121543c
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2018-05-24 20:06:45 +0000

    snekfetch 4.0.0

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const snekfetch = require('snekfetch');
 
 module.exports = async id => {
-	const { text } = await snekfetch.get(`https://www.jam-community.com/song/${id}`);
+	const { raw } = await snekfetch.get(`https://www.jam-community.com/song/${id}`);
+	const text = raw.toString();
 	const [url] = text.match(/https:\/\/mmj-live\.s3-accelerate\.amazonaws\.com\/assets\/music_file\/(.+)\.m4a/);
 	const [, title] = text.match(/<title>(.+)<\/title>/);
 	const { body } = await snekfetch.get(url);
